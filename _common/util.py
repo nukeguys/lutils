27fde6769f6899573ea2dacf4486e3a6c59fb6be
seungkyu.sung <nukeguys0@gmail.com>
2017-08-30 20:30:16 +0900

    Add checkout.py

---
+++ _common/util.py
@@ -0,0 +1,15 @@
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    reverse = dict((value, key) for key, value in enums.items())
+    enums['reverse_mapping'] = reverse
+    return type('Enum', (), enums)
+
+class Colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    DEFAULT = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
