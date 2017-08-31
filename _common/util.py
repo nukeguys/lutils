23db3e5ab6886b375c72b3832d4dc6f7b47e6021
seungkyu.sung <nukeguys0@gmail.com>
2017-08-31 15:02:25 +0900

    Create util class

--- _common/util.py
+++ _common/util.py
@@ -3,21 +3,19 @@ import os
 
 
 class Util:
+    def enum(*sequential, **named):
+        enums = dict(zip(sequential, range(len(sequential))), **named)
+        reverse = dict((value, key) for key, value in enums.items())
+        enums['reverse_mapping'] = reverse
+        return type('Enum', (), enums)
 
 
-def enum(*sequential, **named):
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    reverse = dict((value, key) for key, value in enums.items())
-    enums['reverse_mapping'] = reverse
-    return type('Enum', (), enums)
+    def filesByName(rootDir, fileName):
+        if not os.path.isdir(rootDir):
+            raise EnvironmentError('root is not directory')
 
-
-def filesByName(rootDir, fileName):
-    if not os.path.isdir(rootDir):
-        raise EnvironmentError('root is not directory')
-
-    foundFiles = []
-    for root, subDirs, files in os.walk(rootDir):
-        if fileName in files:
-            foundFiles.append(os.path.join(root, fileName))
-    return foundFiles
+        foundFiles = []
+        for root, subDirs, files in os.walk(rootDir):
+            if fileName in files:
+                foundFiles.append(os.path.join(root, fileName))
+        return foundFiles
